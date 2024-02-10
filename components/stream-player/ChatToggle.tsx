24a3997c67a5c2515682a79315290548cf5a13e4
gyani <g.arya.mallick@gmail.com>
2024-02-10 18:08:13 +0530

    implemented chat component

---
+++ components/stream-player/ChatToggle.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import React from "react";
+import { ArrowLeftFromLine, ArrowRightFromLine } from "lucide-react";
+
+import { Hint } from "@/components/hint";
+import { Button } from "@/components/ui/button";
+import { useChatSidebar } from "@/store/use-chat-sidebar";
+
+const ChatToggle = () => {
+  const { collapsed, onExpand, onCollapse } = useChatSidebar((state) => state);
+
+  const onToggle = () => {
+    if (collapsed) {
+      onExpand();
+    } else {
+      onCollapse();
+    }
+  };
+  const label = collapsed ? "Expand" : "Collapse";
+  const Icon = collapsed ? ArrowLeftFromLine : ArrowRightFromLine;
+  return (
+    <Hint label={label} side="left" asChild>
+      <Button
+        onClick={onToggle}
+        variant="ghost"
+        className="h-auto p-2 hover:bg-white/10 hover:text-primary bg-transparent"
+      >
+        <Icon className="h-4 w-4" />
+      </Button>
+    </Hint>
+  );
+};
+
+export default ChatToggle;
--- components/stream-player/StreamPlayer.tsx
+++ components/stream-player/StreamPlayer.tsx
@@ -5,6 +5,10 @@ import { Stream, User } from "@prisma/client";
 import React from "react";
 import { LiveKitRoom } from "@livekit/components-react";
 import Video from "./Video";
+import { useChatSidebar } from "@/store/use-chat-sidebar";
+import { cn } from "@/lib/utils";
+import Chat from "./Chat";
+import ChatToggle from "./ChatToggle";
 
 interface StreamPlayerProps {
   user: User & { stream: Stream | null };
@@ -13,6 +17,8 @@ interface StreamPlayerProps {
 }
 
 const StreamPlayer = ({ user, stream, isFollowing }: StreamPlayerProps) => {
+  const { collapsed } = useChatSidebar((state) => state);
+
   const { token, name, identity } = useViewerToken(user.id);
   if (!token || !name || !identity) {
     return <div>Cannot watch the stream</div>;
@@ -20,13 +26,32 @@ const StreamPlayer = ({ user, stream, isFollowing }: StreamPlayerProps) => {
 
   return (
     <>
+      {collapsed && (
+        <div className="hidden lg:block fixed top-[100px] right-2 z-50">
+          <ChatToggle />
+        </div>
+      )}
       <LiveKitRoom
         token={token}
         serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_WS_URL}
-        className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full"
+        className={cn(
+          "grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full",
+          collapsed && "lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2"
+        )}
       >
         <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
           <Video hostName={user.username} hostIdentity={user.id} />
+        </div>
+        <div className={cn("col-span-1", collapsed && "hidden")}>
+          <Chat
+            viewerName={name}
+            hostName={user.username}
+            hostIdentity={user.id}
+            isFollowing={isFollowing}
+            isChatEnabled={stream.isChatEnabled}
+            isChatDelayed={stream.isChatDelayed}
+            isChatFollowerOnly={stream.isChatFollowersOnly}
+          />
         </div>
       </LiveKitRoom>
     </>
